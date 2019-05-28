57253aac4a0303468a0290888b4e172e7c617445
Matthew Landauer <mlandauer@gmail.com>
2019-05-28 13:03:21 +1000

    Update to latest atdisplanningalertsfeed gem

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,4 @@ Bundler.require
 
 url = "http://mycouncil2.solorient.com.au/Horizon/@@horizondap_ashfield@@/atdis/1.0/"
 
-ATDISPlanningAlertsFeed.save(url)
+ATDISPlanningAlertsFeed.save(url, "Sydney")
